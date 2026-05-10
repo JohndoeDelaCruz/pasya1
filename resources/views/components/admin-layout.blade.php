b7d232961ddc66cf51e0832cb327b1044fd20d1e
Garousin <baterinalordalfrey8@gmail.com>
2026-05-10 19:59:07 +0800

    v1.5.32 remove non-functional dark mode toggle from admin layout

--- resources/views/components/admin-layout.blade.php
+++ resources/views/components/admin-layout.blade.php
@@ -198,13 +198,6 @@
                             </svg>
                         </button>
 
-                        <!-- Dark mode toggle -->
-                        <button class="text-gray-600 hover:text-gray-900">
-                            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"/>
-                            </svg>
-                        </button>
-
                         <!-- User Account Menu -->
                         <div x-data="{ open: false }" class="relative">
                             <button @click="$dispatch('pasya-show-mobile-header'); open = !open" class="flex items-center text-gray-600 hover:text-gray-900 focus:outline-none">
